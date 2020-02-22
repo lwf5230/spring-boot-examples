3477216120acd87eafac5cbefaf547020838f046
liwenfeng <lwf5230@sina.com>
2020-02-23 02:14:56 +0800

    lombook引入

--- spring-boot-mybatis/spring-boot-mybatis-annotation/src/main/java/com/neo/model/User.java
+++ spring-boot-mybatis/spring-boot-mybatis-annotation/src/main/java/com/neo/model/User.java
@@ -3,7 +3,11 @@ package com.neo.model;
 import java.io.Serializable;
 
 import com.neo.enums.UserSexEnum;
+import lombok.Data;
+import lombok.ToString;
 
+@Data
+@ToString
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -22,52 +26,6 @@ public class User implements Serializable {
 		this.passWord = passWord;
 		this.userName = userName;
 		this.userSex = userSex;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getPassWord() {
-		return passWord;
-	}
-
-	public void setPassWord(String passWord) {
-		this.passWord = passWord;
-	}
-
-	public UserSexEnum getUserSex() {
-		return userSex;
-	}
-
-	public void setUserSex(UserSexEnum userSex) {
-		this.userSex = userSex;
-	}
-
-	public String getNickName() {
-		return nickName;
-	}
-
-	public void setNickName(String nickName) {
-		this.nickName = nickName;
-	}
-
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return "userName " + this.userName + ", pasword " + this.passWord + "sex " + userSex.name();
 	}
 
 }
